295e6ded60899c9fc1a4fa1c1f7368d2cd5fbf88
Jonathan Merritt <j.s.merritt@gmail.com>
2016-02-20 19:24:05 +1100

    Dump hex for testing
    Some utilities for dumping out hex snippets in Haskell format for
    testing purposes.

---
+++ app/MainHexUtil.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE RecordWildCards #-}
+{-|
+Description : Command-line utility for dumping hex (for testing)
+Copyright   : (c) Jonathan Merritt, 2016
+Maintainer  : j.s.merritt@gmail.com
+
+Extracts hex from files in a format suitable for inserting direcly into
+Haskell source files for testing purposes.
+-}
+module Main where
+
+import           Data.HC3D.HexUtil   (HexBlock, encode, haskellText,
+                                      hexdumpText)
+
+import qualified Data.ByteString     as B
+import qualified Data.Text.IO        as TIO
+import           Options.Applicative
+
+
+main :: IO ()
+main = getCmdOptions >>= doExport
+
+
+doExport :: CmdOptions -> IO ()
+doExport CmdOptions{..} = do
+    inbs <- B.readFile inFileName
+    let
+        dn = maybe 0 id skipN
+        bs' = B.drop dn inbs
+        bs = case takeN of
+            Just n  -> B.take n bs'
+            Nothing -> bs'
+        hb = encode dn 16 bs
+        ss = if hexDump
+                 then hexdumpText hb
+                 else haskellText 4 4 hb
+    TIO.putStr ss
+
+
+getCmdOptions :: IO CmdOptions
+getCmdOptions = execParser opts
+  where
+    opts = info (helper <*> cmdOptionsParser)
+        (  fullDesc
+        <> progDesc "Hex dumping utility"
+        <> header "hexutil - dumps hex as inline Haskell."
+        )
+
+
+data CmdOptions = CmdOptions
+    { inFileName :: String
+    , skipN      :: Maybe Int
+    , takeN      :: Maybe Int
+    , hexDump    :: Bool
+    }
+
+
+cmdOptionsParser :: Parser CmdOptions
+cmdOptionsParser = CmdOptions
+    <$> argument str (metavar "FILE")
+    <*> option auto
+        (  long "skip"
+        <> short 's'
+        <> value Nothing
+        <> metavar "offset"
+        <> help "Skip this many bytes from the beginning of the input."
+        )
+    <*> option auto
+        (  long "take"
+        <> short 'n'
+        <> value Nothing
+        <> metavar "length"
+        <> help "Interpret only this many bytes of input."
+        )
+    <*> switch
+        (  long "hexdump"
+        <> help "Export in hexdump canonical format rather than as Haskell."
+        )
+
--- src/Data/HC3D/HexUtil.hs
+++ src/Data/HC3D/HexUtil.hs
@@ -1,98 +1,173 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-|
+Description : Hex Utilities (for testing)
+Copyright   : (c) Jonathan Merritt, 2016
+Maintainer  : j.s.merritt@gmail.com
 
-module Data.HC3D.HexUtil where
+Utility functions for extracting hex that can be inserted directly into
+Haskell source files for testing purposes.
+-}
+module Data.HC3D.HexUtil (
+    -- * Types
+    HexBlock(..)
+  , HexLine(..)
+    -- * Functions
+  , decode
+  , encode
+  , haskellText
+  , hexdumpText
+) where
 
-import Data.ByteString (ByteString)
-import Data.Char
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as C
-import Data.ByteString.Base16
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as TL
-import Data.Word
-import Formatting
+import           Data.ByteString        (ByteString)
+import qualified Data.ByteString        as B
+import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Char8  as C
+import           Data.Char              (isPrint)
+import qualified Data.Text              as T
+import qualified Data.Text.Lazy         as TL
+import           Data.Word              (Word8)
+import           Formatting             (format, left, text, (%))
 
+
+------------------------------------------------------------------------------
+-- DATA TYPES
+------------------------------------------------------------------------------
+
+
+-- | Line of hex from a file.
+--
+--   'HexLine' redundantly represents a line of hex from some source file. It
+--   contains redunant information so that it can be read more conveniently in
+--   tests.
 data HexLine = HexLine
-    { offset :: Int
-    , bytes  :: [Word8]
-    , ascii  :: Text
+    { offset :: Int         -- ^ offset of the first byte within the file
+    , base16 :: ByteString  -- ^ data stored base-16 encoded
+    , ascii  :: T.Text      -- ^ printable ASCII characters
     }
 
-tupleToHexLine :: (Int, [Word8], Text) -> HexLine
-tupleToHexLine (offset, bytes, ascii) = HexLine{..}
 
+-- | Block of hex from a file.
 data HexBlock = HexBlock [HexLine]
 
-bsToHexBlock :: Int
-             -> Int
-             -> ByteString
-             -> HexBlock
-bsToHexBlock initOffset lineWidth bs =
+
+------------------------------------------------------------------------------
+-- EXPORTED FUNCTIONS
+------------------------------------------------------------------------------
+
+
+-- | Decodes a 'HexBlock' into its raw 'ByteString'.
+--
+--   In theory, this can fail. For example, the 'HexBlock' can contain
+--   non-hex characters. However, since 'decode' is used primarily for
+--   testing, it should be valid simply to fail the test if the decode itself
+--   fails.
+decode :: HexBlock -> Maybe ByteString
+decode (HexBlock ls) = fmap B.concat $ sequence $ map decodeLine ls
+
+
+-- | Encodes a 'ByteString' into a 'HexBlock'.
+encode :: Int         -- ^ initial offset of the block (documentation only!)
+       -> Int         -- ^ width of a line of the block in bytes
+       -> ByteString  -- ^ bytestring to encode
+       -> HexBlock    -- ^ produced block
+encode initOffset lineWidth bs =
+
   let
     bss     = chunks lineWidth bs
     offsets = scanl (\n b -> n + B.length b) initOffset bss
-    bytess  = map B.unpack bss
+    base16s = map B16.encode bss
     asciis  = map (T.pack . replaceNonPrintable . C.unpack) bss
-  in
-    HexBlock $ map tupleToHexLine $ zip3 offsets bytess asciis
 
--- Replaces non-printable characters in a String with '.'
+  in
+    HexBlock $ map tupleToHexLine $ zip3 offsets base16s asciis
+
+-- | Converts a 'HexBlock' to 'Text' suitable for inclusion in a Haskell file.
+haskellText :: Int       -- ^ initial indent size
+            -> Int       -- ^ indent width
+            -> HexBlock  -- ^ block to convert
+            -> T.Text    -- ^ text representing the block
+haskellText initIndent indent (HexBlock ls) =
+  let
+    ot  = TL.pack $ replicate initIndent ' '
+    f l = format (text % text)
+              ot
+              (TL.fromStrict $ haskellHexLine indent l)
+    ss  = TL.intercalate "\n" $ map f ls
+
+  in
+    TL.toStrict $
+        format ( text % "HexBlock [\n"
+               % text % "\n"
+               % text % "]\n"
+               ) ot ss ot
+
+-- | Converts a 'HexBlock' to 'Text' in a format similar to the hexdump tool.
+hexdumpText :: HexBlock -> T.Text
+hexdumpText (HexBlock ls) =
+  let
+    nColBytes = maximum $ map (T.length . ascii) ls
+  in
+    T.intercalate "\n" $ map (hexdumpHexLine nColBytes) ls
+
+
+------------------------------------------------------------------------------
+-- INTERNAL FUNCTIONS
+------------------------------------------------------------------------------
+
+
+-- | Decodes a line of hex.
+decodeLine :: HexLine -> Maybe ByteString
+decodeLine hl = if (B.null . snd) d then Just (fst d) else Nothing
+  where
+    d = B16.decode $ base16 hl
+
+
+-- | Constructs a HexLine from a tuple.
+tupleToHexLine :: (Int, ByteString, T.Text) -> HexLine
+tupleToHexLine (offset, base16, ascii) = HexLine{..}
+
+
+-- | Replaces non-printable characters in a String with '.'
 replaceNonPrintable :: String -> String
 replaceNonPrintable ss = map (\x -> if isPrint x then x else '.') ss
 
--- Split into chunks of length n
+
+-- | Split into chunks of length n
 chunks :: Int -> ByteString -> [ByteString]
 chunks n bs
     | C.null bs = []
     | otherwise = C.take n bs : chunks n (C.drop n bs)
 
-haskellPrintHexBlock :: Int -> Int -> HexBlock -> Text
-haskellPrintHexBlock initIndent indent (HexBlock ls) =
-  let
-    ot  = TL.pack $ replicate initIndent ' '
-    f l = format (text % text) 
-              ot 
-              (TL.fromStrict $ haskellPrintHexLine indent l)
-    ss  = TL.intercalate "\n" $ map f ls
-  in
-    TL.toStrict $
-        format ( text % "HexBlock [\n"
-               % text
-               % text % "]\n"
-               ) ot ss ot
 
-haskellPrintHexLine :: Int -> HexLine -> Text
-haskellPrintHexLine indent HexLine{..} =
+-- | Prints a 'HexLine' in a format suitable for a Haskell file.
+haskellHexLine :: Int -> HexLine -> T.Text
+haskellHexLine indent HexLine{..} =
   let
     dt = TL.pack $ replicate indent ' '
     os = format (left 6 '0') offset
-    bs = TL.intercalate ", " $ map (format ("0x" % hex)) bytes
+    bs = TL.pack $ C.unpack base16
     ss = format (text) $ TL.fromStrict ascii
   in
     TL.toStrict $
         format ( text
-               % "HexLine " 
+               % "HexLine "
                % text
-               % "[ " % text % "] \""
-               % text 
+               % " \""
+               % text
+               % "\" \""
+               % text
                % "\""
                ) dt os bs ss
 
-prettyPrintHexBlock :: HexBlock -> Text
-prettyPrintHexBlock (HexBlock ls) =
-  let
-    nColBytes = maximum $ map (T.length . ascii) ls
-  in
-    T.intercalate "\n" $ map (prettyPrintHexLine nColBytes) ls
 
-prettyPrintHexLine :: Int -> HexLine -> Text
-prettyPrintHexLine nColBytes HexLine{..} =
+-- | Prints a 'HexLine' in a format suitable for hexdump.
+hexdumpHexLine :: Int -> HexLine -> T.Text
+hexdumpHexLine nColBytes HexLine{..} =
   let
     es = TL.concat $ replicate (nColBytes - T.length ascii) "   "
     os = format (left 6 '0' % ":") offset
-    bs = TL.concat $ map (format (" " % hex)) bytes
+    bs = TL.intercalate " " $ map (TL.pack . C.unpack) $ chunks 2 base16
     ss = format (": " % text) $ TL.fromStrict ascii
   in
     TL.toStrict $
